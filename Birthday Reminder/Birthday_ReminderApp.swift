82dc8719e9c869576efc729043510253a33d6bc4
Timo Reusch <46966275+TimoReusch@users.noreply.github.com>
2021-09-17 23:51:00 +0200

    Initial Commit

---
+++ Birthday Reminder/Birthday_ReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Birthday_ReminderApp.swift
+//  Birthday Reminder
+//
+//  Created by Timo Reusch on 17.09.21.
+//
+
+import SwiftUI
+
+@main
+struct Birthday_ReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Birthday Reminder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Birthday Reminder
+//
+//  Created by Timo Reusch on 17.09.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
