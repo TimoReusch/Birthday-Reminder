92898fa31043600a1db3680009fd08655183c27b
Timo Reusch <46966275+TimoReusch@users.noreply.github.com>
2021-10-11 16:11:50 +0200

    Removing unnecessary Save-Button

--- Birthday Reminder/Views/BirthdayDetail.swift
+++ Birthday Reminder/Views/BirthdayDetail.swift
@@ -20,19 +20,6 @@ struct BirthdayDetail: View {
             Section(header: Text("Notes")){
                 TextEditor(text: $birthday.notes)
             }
-            Section{
-                Button("Save"){
-                    if(!birthday.name.elementsEqual(self.birthday.name)){
-                        birthday.name = self.birthday.name
-                    }
-                    if(birthday.date != self.birthday.date){
-                        birthday.date = self.birthday.date
-                    }
-                    if(!birthday.notes.elementsEqual(self.birthday.notes)){
-                        birthday.notes = self.birthday.notes
-                    }
-                }
-            }
         }
         .navigationTitle(birthday.name)
     }
