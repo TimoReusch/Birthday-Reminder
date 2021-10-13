e0c817d4a8080111eb7f8fea241a28c75b074653
Timo Reusch <46966275+TimoReusch@users.noreply.github.com>
2021-10-14 00:16:14 +0200

    Created formatter for displaying Birthday

---
+++ Birthday Reminder/Data/Date/BirthdayDateFormatter.swift
@@ -0,0 +1,14 @@
+//
+//  BirthdayDateFormatter.swift
+//  Birthday Reminder
+//
+//  Created by Timo Reusch on 13.10.21.
+//
+
+import Foundation
+
+func birthdayDateFormatter(date: Date) -> String {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "d MMM yyyy"
+    return dateFormatter.string(from: date)
+}
