b51c2f319b8a192e34440a74284dffc4a44136bb
Timo Reusch <46966275+TimoReusch@users.noreply.github.com>
2021-10-16 22:06:28 +0200

    Better Test data

--- Birthday Reminder/Data/Birthday.swift
+++ Birthday Reminder/Data/Birthday.swift
@@ -11,6 +11,7 @@ struct Birthday: Identifiable, Equatable {
     var id = UUID()
     var name: String
     var date: Date
+    var daysTillBirthday: Int = 0
     var notes: String = ""
     
     static func == (lhs: Birthday, rhs: Birthday) -> Bool {
@@ -20,7 +21,18 @@ struct Birthday: Identifiable, Equatable {
 }
 
 let testData = [
-    Birthday(name: "Laura", date: Date(), notes: "Chocolate"),
-    Birthday(name: "Timo", date: Date())
+    Birthday(name: "Cosi", date: createDate(year: 1999, month: 12, day: 20)),
+    Birthday(name: "Laura", date: createDate(year: 2000, month: 2, day: 15), notes: "Chocolate"),
+    Birthday(name: "Patrick", date: createDate(year: 1999, month: 8, day: 29)),
+    Birthday(name: "Carl", date: Date()),
+    Birthday(name: "Tom", date: createDate(year: 2021, month: 12, day: 31))
 ]
 
+private func createDate(year: Int, month: Int, day: Int)->Date{
+    var components = DateComponents()
+    components.year = year
+    components.month = month
+    components.day = day
+    
+    return Calendar.current.date(from: components)!
+}
