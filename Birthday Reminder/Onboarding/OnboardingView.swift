79ba6729abb69e3988e560b3263467c6871f0822
Timo Reusch <46966275+TimoReusch@users.noreply.github.com>
2021-12-26 02:29:15 +0100

    Onboarding

---
+++ Birthday Reminder/Onboarding/OnboardingView.swift
@@ -0,0 +1,83 @@
+//
+//  OnboardingView.swift
+//  Birthday Reminder
+//
+//  Created by Timo Reusch on 26.12.21.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    @Binding var shouldShowOnboarding: Bool
+    
+    var body: some View {
+        TabView{
+            VStack{
+                Spacer()
+                LottieView(filename: "birthdayAnimation", loop: true).frame(height: 300, alignment: .center)
+                Text("Hey!")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading)
+                    .padding(.top)
+                    .font(.title)
+                Text("So great to meet you!")
+                    .font(.title2)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading)
+                    .padding(.bottom)
+                
+                Text("Birthday Reminder helps you to never forget a birthday again - store all your friends most important day in one place with ease. Plus, there is also room for present ideas.")
+                    .padding()
+                Spacer()
+            }
+            VStack{
+                Spacer()
+                LottieView(filename: "dataPrivacyAnimation", loop: false).frame(height: 300, alignment: .center)
+                Text("Yours is yours.")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading)
+                    .padding(.top)
+                    .font(.title)
+                Text("Data Privacy")
+                    .font(.title2)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading)
+                    .padding(.bottom)
+                
+                Text("We do not have access to your data at any time. Everything is stored on your own device, and should you ever loose it or switch to a new one - a backup is automatically created in your iCloud. This also allows you to use the app on all devices you are signed in with your Apple-ID.")
+                    .padding()
+                Spacer()
+            }
+            VStack{
+                Spacer()
+                LottieView(filename: "notificationAnimation", loop: false).frame(height: 300, alignment: .center)
+                Text("We will remind you!")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading)
+                    .padding(.top)
+                    .font(.title)
+                Text("Notifications")
+                    .font(.title2)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading)
+                    .padding(.bottom)
+                
+                Text("To remind you of your friends birthdays, we would like to send you notifications. Just click on the button below.")
+                    .padding()
+                if #available(iOS 15.0, *) {
+                    Button("Allow notifications"){
+                        // Notification
+                        shouldShowOnboarding.toggle()
+                    }
+                    .buttonStyle(.borderedProminent)
+                    Spacer()
+                } else {
+                    // Fallback on earlier versions
+                }
+                Button("Nah, don't want any notifications."){ shouldShowOnboarding.toggle() }
+                Spacer()
+            }
+        }
+        .tabViewStyle(PageTabViewStyle())
+    }
+}
